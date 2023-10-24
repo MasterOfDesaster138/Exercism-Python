6c6a395914b4461f188b1b98737992d5f61d16ac
MasterOfDesaster138 <83144811+MasterOfDesaster138@users.noreply.github.com>
2023-10-24 14:32:43 +0200

    solved square-roots

--- python-track/square-root/square_root.py
+++ python-track/square-root/square_root.py
@@ -1,2 +1,10 @@
 def square_root(number):
-    pass
+    estimation = 1
+    while True:
+        if estimation * estimation != number:
+            square_number = number / estimation 
+            estimation = (estimation + square_number) / 2
+        else:
+            if estimation * estimation == number:
+                return estimation
+        
