c1c6372d65a541c4470bf0d1ab35d9744df20033
MasterOfDesaster138 <83144811+MasterOfDesaster138@users.noreply.github.com>
2023-11-05 23:51:56 +0100

    refactoring

--- python-track/rotational-cipher/rotational_cipher.py
+++ python-track/rotational-cipher/rotational_cipher.py
@@ -1,6 +1,6 @@
 def rotate(text: str, key: int) -> str:
     import string
-    alphabet: str = string.ascii_lowercase
+    ALPHABET: str = string.ascii_lowercase
     cipher: str = ""
     
     for char in text:
@@ -8,24 +8,10 @@ def rotate(text: str, key: int) -> str:
         if char.isalpha():
             # separate upper and lower case letters
             if char.isupper():
-                # get an index of the current character and the cipher character
-                char_position: int = alphabet.find(char.lower())  
-                char_index: int = char_position + key
-                # check if new index is in range of the alphabet_string
-                if char_index > len(alphabet):
-                    char_index = char_index - len(alphabet) 
-                
-                cipher += alphabet[char_index].upper()
+                cipher += ALPHABET[(ALPHABET.index(char.lower()) + key) % 26].upper()
                 
             else:
-                # get an index of the current character and the cipher character
-                char_position: int = alphabet.find(char)  
-                char_index: int = char_position + key
-                # check if new index is in range of the alphabet_string
-                if char_index > len(alphabet) - 1:
-                    char_index = char_index - len(alphabet) 
-                
-                cipher += alphabet[char_index]
+                cipher += ALPHABET[(ALPHABET.index(char) + key) % 26]
                 
         else:
             cipher += char
