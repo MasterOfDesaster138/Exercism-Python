95dc66e28ec131d578fafbb0a8d992a71e7e404e
MasterOfDesaster138 <83144811+MasterOfDesaster138@users.noreply.github.com>
2024-01-06 02:37:18 +0100

    in progress

--- python-track/spiral-matrix/spiral_matrix_test.py
+++ python-track/spiral-matrix/spiral_matrix_test.py
@@ -23,8 +23,7 @@ class SpiralMatrixTest(unittest.TestCase):
         self.assertEqual(spiral_matrix(3), [[1, 2, 3], [8, 9, 4], [7, 6, 5]])
 
     def test_spiral_of_size_4(self):
-        self.assertEqual(
-            spiral_matrix(4),
+        self.assertEqual(spiral_matrix(4),
             [[1, 2, 3, 4], [12, 13, 14, 5], [11, 16, 15, 6], [10, 9, 8, 7]],
         )
 
